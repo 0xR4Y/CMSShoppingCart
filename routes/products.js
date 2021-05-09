8bf7e64dec5f915761b0032a26c34c3c1fb98e68
Ramin Roufeh <ramin1408@gmail.com>
2021-05-09 12:30:44 -0700

    2nd commit

--- routes/products.js
+++ routes/products.js
@@ -3,12 +3,40 @@ const router = express.Router();
 
 const Product = require("../models/product");
 
-//Get pages
+//Get /products
 router.get("/", (req,res)=>{
     Product.find({},(error,products)=>{
         if(error) console.log(error);
         res.json(products);
     });
 });
+
+//Get /products/category
+router.get("/:category", (req,res)=>{
+    const cat = req.params.category;
+
+    Product.find({ category: cat},(error,products)=>{
+        if(error) console.log(error);
+        res.json(products);
+    });
+});
+
+//Get /count/category
+router.get("/count/:category", (req,res)=>{
+    const cat = req.params.category;
+
+    if(cat === "all"){
+        Product.countDocuments({}, (error,count) => res.json(count));
+    } else{
+        Product.countDocuments({ category: cat}, (error,count) => 
+        res.json(count)
+        );
+    }
+
+});
+
+
+
+
 
 module.exports = router;
