a8479b5e2838a6a1472fd363fff402d699e339a2
Ramin Roufeh <ramin1408@gmail.com>
2021-05-13 09:14:30 -0700

    First commit

--- routes/products.js
+++ routes/products.js
@@ -5,25 +5,28 @@ const Product = require("../models/product");
 
 //Get /products
 router.get("/", (req,res)=>{
+    const p = req.query.p ? req.query.p :1;
     Product.find({},(error,products)=>{
         if(error) console.log(error);
         res.json(products);
-    });
+    }).skip((p-1)*4).limit(4);
 });
 
 //Get /products/category
 router.get("/:category", (req,res)=>{
     const cat = req.params.category;
+    const p = req.query.p ? req.query.p :1;
 
     Product.find({ category: cat},(error,products)=>{
         if(error) console.log(error);
         res.json(products);
-    });
+    }).skip((p-1)*4).limit(4);
 });
 
 //Get /count/category
 router.get("/count/:category", (req,res)=>{
     const cat = req.params.category;
+    const p = req.query.p ? req.query.p :1;
 
     if(cat === "all"){
         Product.countDocuments({}, (error,count) => res.json(count));
